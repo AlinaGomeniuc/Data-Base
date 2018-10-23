5858c2d6d144fc2961ac755e3407d4847f633dbd
AlinaGomeniuc <33789677+AlinaGomeniuc@users.noreply.github.com>
2018-10-23 22:05:22 +0300

    Delete SQLQuery_ex24.sql

--- Lab4/CodeSQL/SQLQuery_ex24.sql
+++
@@ -1,5 +0,0 @@
-select Disciplina, COUNT(distinct studenti_reusita.Id_Profesor) as Nr_profesori
-from discipline, studenti_reusita,profesori
-where discipline.Id_Disciplina = studenti_reusita.Id_Disciplina
-group by Disciplina
-having count(distinct studenti_reusita.Id_Profesor) > 1
