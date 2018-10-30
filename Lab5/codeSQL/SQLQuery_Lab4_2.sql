e710e0636be2d4c780785b16b37a2910bfba5a99
AlinaGomeniuc <33789677+AlinaGomeniuc@users.noreply.github.com>
2018-10-30 22:09:49 +0200

    Add files via upload

---
+++ Lab5/codeSQL/SQLQuery_Lab4_2.sql
@@ -0,0 +1,29 @@
+declare @Nume_Disciplina varchar(20) = 'Baze de date';
+declare @Tipul_Testului varchar(20) = 'Testul 1';
+declare @Nota1 int = 6;
+declare @Nota2 int = 8;
+
+if @Nota1 !=any (select  top (10) Nota
+from studenti, studenti_reusita, discipline
+where studenti.Id_Student = studenti_reusita.Id_Student
+and Disciplina = @Nume_Disciplina
+and Tip_Evaluare = @Tipul_Testului)
+
+and @Nota2 != any (select  top (10) Nota
+from studenti, studenti_reusita, discipline
+where studenti.Id_Student = studenti_reusita.Id_Student
+and Disciplina = @Nume_Disciplina
+and Tip_Evaluare = @Tipul_Testului)
+
+begin
+
+select  top (10) Nume_Student, Prenume_Student, Nota
+from studenti, studenti_reusita, discipline
+where discipline.Id_Disciplina = studenti_reusita.Id_Disciplina
+and studenti.Id_Student = studenti_reusita.Id_Student
+and Disciplina = @Nume_Disciplina
+and Tip_Evaluare = @Tipul_Testului
+and Nota not in (@Nota1, @Nota2)
+
+end
+
