f83e3fe573a53308db3dfc3504496c83069b1a89
AlinaGomeniuc <33789677+AlinaGomeniuc@users.noreply.github.com>
2018-11-06 21:57:47 +0200

    Update SQLQuery_Lab6_ex3-2.sql

--- LAB6/SQLcode/SQLQuery_Lab6_ex3-2.sql
+++ LAB6/SQLcode/SQLQuery_Lab6_ex3-2.sql
@@ -9,12 +9,12 @@ DECLARE @index int = 1;
 while (@index <= @nr_grupe)
 begin
 	UPDATE grupe
-	--set Sef_grupa = (select  x.Id_Student
-					 -- from (select Id_Student , avg(cast(Nota as float)) as Media
-						--	from studenti_reusita
-							--where Id_Grupa = @index
-							--group by  Id_Student
-							--having avg(cast(Nota as float)) >= all(select avg(cast(Nota as float))
+	set Sef_grupa = (select  x.Id_Student
+					  from (select Id_Student , avg(cast(Nota as float)) as Media
+						from studenti_reusita
+						where Id_Grupa = @index
+						group by  Id_Student
+						having avg(cast(Nota as float)) >= all(select avg(cast(Nota as float))
 																	--from studenti_reusita
 																	--where Id_Grupa = @index
 																	--group by Id_Student)  
@@ -22,10 +22,10 @@ begin
 			
 		set Prof_Indrumator = (select  y.Id_Profesor
 					  from (select Id_Profesor , count(distinct Id_Disciplina) as Nr_de_discipline
-							from studenti_reusita
-							where Id_Grupa = @index
-							group by  Id_Profesor
-							having count(distinct Id_Disciplina) >= all(select count(distinct Id_Disciplina)
+					  from studenti_reusita
+					  where Id_Grupa = @index
+					  group by  Id_Profesor
+					  having count(distinct Id_Disciplina) >= all(select count(distinct Id_Disciplina)
 																	from studenti_reusita
 																	where Id_Grupa = @index
 																	group by Id_Profesor)  
