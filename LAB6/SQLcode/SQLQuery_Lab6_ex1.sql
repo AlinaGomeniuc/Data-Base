8e58a4b7abf553cdb3a652b70fd89a9f7bd7c495
AlinaGomeniuc <33789677+AlinaGomeniuc@users.noreply.github.com>
2018-11-06 18:18:02 +0200

    Add files via upload

---
+++ LAB6/SQLcode/SQLQuery_Lab6_ex1.sql
@@ -0,0 +1,5 @@
+UPDATE profesori set Adresa_Postala_Profesor = 'mun.Chisinau'
+				 where Adresa_Postala_Profesor IS NULL;
+
+SELECT Nume_Profesor, Prenume_Profesor, Adresa_Postala_Profesor
+FROM profesori
