88c45be19c3a85dedb243b4eeaba39324f7b0e4f
AlinaGomeniuc <33789677+AlinaGomeniuc@users.noreply.github.com>
2018-11-19 20:03:26 +0200

    Add files via upload

---
+++ Lab8/SQLcode/SQLQuery_Lab8_ex4_1.sql
@@ -0,0 +1,14 @@
+ALTER VIEW View_ex1_Lab8 WITH SCHEMABINDING AS
+SELECT  studenti.studenti.Id_Student, Nume_Student , Prenume_Student
+FROM studenti.studenti, plan_studii.discipline , studenti.studenti_reusita
+Where studenti.studenti.Id_Student = studenti.studenti_reusita.Id_Student
+and plan_studii.discipline.Id_Disciplina = studenti.studenti_reusita.Id_Disciplina
+and studenti.studenti_reusita.Tip_Evaluare = 'examen' 
+and year(studenti.studenti_reusita.Data_Evaluare) = 2018 
+and plan_studii.discipline.Disciplina = 'Baze de date'
+and studenti.studenti_reusita.Nota between  4 and 8
+WITH CHECK OPTION;
+
+GO
+
+ALTER TABLE studenti.studenti DROP COLUMN Nume_Student
